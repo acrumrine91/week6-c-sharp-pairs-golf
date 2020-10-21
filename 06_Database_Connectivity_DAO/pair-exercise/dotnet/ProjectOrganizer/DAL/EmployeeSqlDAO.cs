ee0f58352cb754515149f1d4ff66393b7ccf2ad4
Greg <gregsentai@gmail.com>
2020-10-20 23:58:05 -0400

    almost richard dunn

--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/EmployeeSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/EmployeeSqlDAO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ProjectOrganizer.DAL
 {
@@ -23,7 +24,59 @@ namespace ProjectOrganizer.DAL
         /// <returns>A list of all employees.</returns>
         public IList<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            //Delcare the output variable
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                // Create a connection to the database
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    // Open the connection
+                    conn.Open();
+
+                    // Create a command to send to the database
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM employee;", conn);
+
+                    // Execute the command
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //read each row
+                    while (reader.Read())
+                    {
+                        Employee employee = ConvertReaderToEmployee(reader);
+                        employees.Add(employee);
+                    }
+
+
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error occurred reading Employee.");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return employees;
+        }
+
+        private Employee ConvertReaderToEmployee(SqlDataReader reader)
+        {
+            Employee employee = new Employee();
+
+            employee.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+            employee.DepartmentId = Convert.ToInt32(reader["department_id"]);
+            employee.FirstName = Convert.ToString(reader["first_name"]);
+            employee.LastName = Convert.ToString(reader["last_name"]);
+            employee.JobTitle = Convert.ToString(reader["job_title"]);
+            employee.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+            employee.Gender = Convert.ToString(reader["gender"]);
+            employee.HireDate = Convert.ToDateTime(reader["hire_date"]);
+
+
+
+            return employee;
         }
 
         /// <summary>
@@ -35,7 +88,36 @@ namespace ProjectOrganizer.DAL
         /// <returns>A list of employees that match the search.</returns>
         public IList<Employee> Search(string firstname, string lastname)
         {
-            throw new NotImplementedException();
+
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    // column    // param name  
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Employee WHERE first_name = @first_name AND last_name = @last_name;", conn);
+                    // param name    // param value
+                    cmd.Parameters.AddWithValue("@first_name", firstname);
+                    cmd.Parameters.AddWithValue("@last_name", lastname);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Employee employee = ConvertReaderToEmployee(reader);
+                        employees.Add(employee);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error occurred reading Employees by First and Last Name.");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            return employees;
         }
 
         /// <summary>
@@ -44,7 +126,34 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Employee> GetEmployeesWithoutProjects()
         {
-            throw new NotImplementedException();
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    // column    // param name  
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM employee LEFT JOIN project_employee ON project_employee.employee_id = employee.employee_id WHERE project_id IS NULL", conn);
+                    // param name    // param value
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Employee employee = ConvertReaderToEmployee(reader);
+                        employees.Add(employee);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error occurred reading Employees by First and Last Name.");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            return employees;
         }
     }
 }
--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/ProjectSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/ProjectSqlDAO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ProjectOrganizer.DAL
 {
@@ -23,8 +24,60 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Project> GetAllProjects()
         {
-            throw new NotImplementedException();
+            List<Project> projects = new List<Project>();
+
+            try
+            {
+                // Create a connection to the database
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    // Open the connection
+                    conn.Open();
+
+                    // Create a command to send to the database
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM project;", conn);
+
+                    // Execute the command
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //read each row
+                    while (reader.Read())
+                    {
+                        Project project = ConvertReaderToProject(reader);
+                        projects.Add(project);
+                    }
+
+
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error occurred reading Project.");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return projects;
         }
+
+        private Project ConvertReaderToProject(SqlDataReader reader)
+        {
+            Project project = new Project();
+
+            project.ProjectId = Convert.ToInt32(reader["project_id"]);
+            project.Name = Convert.ToString(reader["name"]);
+            project.StartDate = Convert.ToDateTime(reader["from_date"]);
+            project.EndDate = Convert.ToDateTime(reader["to_date"]);
+
+
+
+            return project;
+        }
+
+
+
+
+
 
         /// <summary>
         /// Assigns an employee to a project using their IDs.
@@ -55,7 +108,35 @@ namespace ProjectOrganizer.DAL
         /// <returns>The new id of the project.</returns>
         public int CreateProject(Project newProject)
         {
-            throw new NotImplementedException();
+            int id = 0;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO project VALUES (@name, @from_date, @to_date);", conn);
+                    cmd.Parameters.AddWithValue("@name ", newProject.Name);
+                    cmd.Parameters.AddWithValue("@from_date ", newProject.StartDate);
+                    cmd.Parameters.AddWithValue("@to_date ", newProject.EndDate);
+
+
+                    cmd.ExecuteNonQuery();
+
+                    // Now print the new Project Id
+                    cmd = new SqlCommand("SELECT MAX(project_id) FROM project;", conn);
+                    id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    Console.WriteLine($"The new project id is {id}");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error saving project.");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return id;
         }
 
     }
