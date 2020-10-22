02141caa3a6f1d998f1a9003d4eb908e3faeb19e
Greg <gregsentai@gmail.com>
2020-10-22 14:05:08 -0400

    started to list venue method

---
+++ 09_Capstone/Capstone/DAL/VenueSqlDAO.cs
@@ -0,0 +1,57 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Capstone.DAL
+{
+    public class VenueSqlDAO
+    {
+        private string connectionString;
+
+        public VenueSqlDAO(string databaseConnectionString)
+        {
+            connectionString = databaseConnectionString;
+        }
+
+        public IList<Venue> GetVenues()
+        {
+            List<Venue> venues = new List<Venue>();
+
+            // maybe we are going to add a trycatch
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM venue;", conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Venue venyou = ConvertReaderToTypeVenue(reader);
+                    venues.Add(venyou);
+                }
+
+            }
+            return venues;
+
+        }
+
+        private Venue ConvertReaderToTypeVenue(SqlDataReader reader)
+        {
+            Venue venyou = new Venue();
+
+            venyou.Id = Convert.ToInt32(reader["id"]);
+            venyou.Name = Convert.ToString(reader["name"]);
+            venyou.CityId = Convert.ToInt32(reader["city_id"]);
+            venyou.Description = Convert.ToString(reader["description"]);
+
+            return venyou;
+        }
+
+        //(id, name, city_id, description)
+    }
+}
--- 09_Capstone/Capstone/UserInterface.cs
+++ 09_Capstone/Capstone/UserInterface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -18,8 +19,71 @@ namespace Capstone
 
         public void Run()
         {
-            Console.WriteLine("Reached the User Interface.");
-            Console.ReadLine();
+            Console.WriteLine("Welcome to Excelsior Venue Services.");
+            Console.WriteLine("You can use this system to seek and book our available spaces.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("~==MAIN MENU==~");
+            Console.WriteLine("Please select from the menu below.");
+            Console.WriteLine("");
+            Console.WriteLine("1) List Venues");
+            Console.WriteLine("Q) or any other key to quit");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                ListVenues();
+            }
+            else return;
         }
+
+        public void ListVenues()
+        {
+            IList<Venue> venues = venueDAO.ListVenues();
+            Console.WriteLine("This is the view venues menu");
+            for (int index = 0; index < venues.Count; index++)
+            {
+                Console.WriteLine(index + ") - " + venues[index]);
+
+              //  Console.WriteLine(Venue.Name);
+            }
+            
+            Console.WriteLine("R) or press any other key to return to main menu");
+            string userInput = Console.ReadLine();
+
+            
+          
+        }
+
+        public void VenuDetailsMenu(string userInput)
+        {
+            //GetVenueDetails(); Method to call details listed below
+
+
+            //Console.WriteLine("name of venue"); // use connection string to use name selected
+            //Console.WriteLine("Location: (city name), (city state abbrev)"); // use city and city state associated with venue selected
+            //Console.WriteLine("Categories: "); //(categories associated with venue selected)
+            //Console.WriteLine("");
+            //Console.WriteLine("venue description"); // use description associated with the venue
+            //Console.WriteLine("");
+            //Console.WriteLine("1) View Venue Spaces");
+            //Console.WriteLine("2) Search for Reservation"); (might BE A BONE US)
+            //Console.WriteLine("R) or press any other key to return to the list of venues");
+            //string nextInput = Console.ReadLine();
+
+        }
+
+        public void ListVenueSpacesMenu()
+        {
+            Console.WriteLine("Name of venue space they selected");
+
+            //list of spaces with info goes here
+
+
+            Console.WriteLine("What would you like to do next?");
+            Console.WriteLine("1) Reserve a space");
+
+        }
+
     }
 }
