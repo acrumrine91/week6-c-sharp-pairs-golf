dbd07cff64ab4ff3ae550af053ead6b0054df709
Austin <Acrumrine91@yahoo.com>
2020-10-24 14:35:40 -0400

    pulling

--- 09_Capstone/Capstone.Tests/CitySqlDAOTests.cs
+++ 09_Capstone/Capstone.Tests/CitySqlDAOTests.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Capstone.DAL;
+using Capstone.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Transactions;
 
 namespace Capstone.Tests
 {
-    class CitySqlDAOTests
+    [TestClass]
+    public class CitySqlDAOTests
     {
+        protected string ConnectionString { get; } = "Data Source=.\\sqlexpress;Initial Catalog=excelsior_venues;Integrated Security=True";
+
+        private TransactionScope transaction;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            transaction = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod]
+        [DataRow(1, 7)]
+        [DataRow(2, 4)]
+        [DataRow(3, 4)]
+        public void GetVenueSpacesShouldReturnAllVenueSpaces(int venueNum, int expectedCount)
+        {
+            //Arrange
+            SpaceSqlDAO dao = new SpaceSqlDAO(ConnectionString);
+
+            //Act
+            IList<Space> spaces = dao.GetVenueSpaces(venueNum);
+
+            //Assert
+            Assert.AreEqual(expectedCount, spaces.Count);
+        }
     }
 }
